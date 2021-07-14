16e58518cd234b0d14749f40cd4f7250a4e297bf
DarkerSU <darkersu@163.com>
2021-07-14 17:49:30 +0800

    init

--- docs/blog/sidebar.js
+++ docs/blog/sidebar.js
@@ -1,4 +1,16 @@
 const { title } = require("../.vuepress/config");
 
 module.exports = [
+    {
+        title: "webpack配置相关",
+        collapsable: true,
+        children: [{ title: "webpack相关", path: "/blog/notes/webpack相关", },
+        ],
+    },
+    {
+        title: "Vue",
+        collapsable: true,
+        children: [{ title: "Vue开发技巧", path: "/blog/notes/Vue开发技巧", },
+        ],
+    },
 ];
